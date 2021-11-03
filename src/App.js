2f78b2ae300708cfbd9196eb8b35f092f75b47f9
Oliver Lundin <79059027+olvard@users.noreply.github.com>
2021-11-03 11:40:48 +0100

    added pomodoro to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css'
 import Schedule from './components/Schedule'
 import CSVReader from 'react-csv-reader'
+import Pomodoro from './components/Pomodoro'
 
 const App = () => {
   return (
     <div>
       <CSVReader onFileLoaded={(data, fileInfo, originalFile) => console.dir(data, fileInfo, originalFile)} />
-
+      <Pomodoro/>
     </div>
   )
 }
---
+++ src/components/Pomodoro.js
@@ -0,0 +1,11 @@
+// Component
+const Pomodoro = () => {
+	return (
+	  <div>
+		<h1>timer</h1>
+	  </div>
+	)
+  }
+  
+  export default Pomodoro
+  
