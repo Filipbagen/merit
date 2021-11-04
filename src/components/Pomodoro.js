b7168e0e78e0d176fb16389693cd0794064d4b3c
Oliver Lundin <79059027+olvard@users.noreply.github.com>
2021-11-04 10:56:26 +0100

    Update Pomodoro.js

--- src/components/Pomodoro.js
+++ src/components/Pomodoro.js
@@ -1,36 +1,74 @@
 import { useEffect, useState } from "react"
 import useInterval from "../misc/useInterval";
+import styled from "styled-components";
+ 
+const AllButts = styled.div`
+	flex-direction: column;
+`
+const Drawer = styled.div`
+	background-color: #F6F6F6;
+	position: absolute;
+	left: 0;
+	right: 0;
+	bottom: 0;
+	height: 100px;
+`
+
+const TimerButts = styled.div`
+	color: white;
+	background-color: #12293B;
+	border: none;
+	border-radius: 10;
+`
+const Butts = styled.div`
+	display: flex;
+	justify-content: space-around;
+	border-radius: 10;
+	
+
+`
+
 
 // Component
 const Pomodoro = () => {
 
 	const [seconds, setSeconds] = useState(25*60);
 	const [message, setDisplayMessage] = useState(false);
+	const [isRunning, setIsRunning]=useState(true);
+	const[delay, setDelay]=useState(1000);
 
 	useInterval(() => {
 		// Your custom logic here
 		if(seconds -1 < 1){
-			setSeconds(0)
+			setSeconds(5*60)
+			setDisplayMessage(true)
 		}else{
 		setSeconds(seconds - 1);
 		}
-	  }, 1000);
+	  },isRunning ? delay : null);
 
-	const timerMinutes = Math.floor(seconds/60);
-	const timerSeconds = seconds%60
+	const timerMinutes = Math.floor(seconds/60) < 10 ? "0" + Math.floor(seconds/60) : Math.floor(seconds/60);
+	const timerSeconds = seconds%60 < 10 ? "0" + seconds%60 : seconds%60;
 
 	return (
-		<div>
-			<div>
-				<button onClick={()=>setSeconds(25*60)}>Nollställ</button>
-				<br></br>
-				<button>Pause</button>
-				<br></br>
-				<button onClick={()=>setSeconds(7)}>Ställ in</button>
-			</div>
+		<AllButts>
 			{message && <div>Vila</div>}
 			{timerMinutes}:{timerSeconds}
-		</div>
+			<Drawer>
+			<div>
+				<Butts>
+					<TimerButts onClick={()=>setSeconds(25*60)}>Nollställ</TimerButts>
+
+					<TimerButts onClick={()=>setSeconds(7)}>Ställ in</TimerButts>
+				
+					<TimerButts onClick={()=>setIsRunning(false)}>Pause</TimerButts>
+					
+					<TimerButts onClick={()=>setIsRunning(true)}>Start</TimerButts>
+				</Butts>
+			</div>
+			</Drawer>
+			
+		</AllButts>
 	  
 	)
   }
