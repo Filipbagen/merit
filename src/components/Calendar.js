4f3b2d41e1ef91e798d2991b96bd386ff4a70ed8
Oliver Lundin <79059027+olvard@users.noreply.github.com>
2021-12-01 21:24:07 +0100

    finalized calendar

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,5 +1,5 @@
-import styled from 'styled-components'
-import React from 'react'
+import styled from "styled-components";
+import React from "react";
 
 const Circle = styled.div`
   display: flex;
@@ -10,38 +10,42 @@ const Circle = styled.div`
   width: 50px;
   height: 50px;
   margin-left: 20px;
-  background-color: ${props => props.color};
+  background-color: ${(props) => props.color};
   font-family: "Barlow", sans-serif;
   font-size: 16px;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-`
+`;
 
 const Week = styled.div`
   display: flex;
-`
+`;
 
-const weekdays = ['Sön', 'Mån', 'Tis', 'Ons', 'Tor', 'Fre', 'Lör']
+const weekdays = ["Sön", "Mån", "Tis", "Ons", "Tor", "Fre", "Lör"];
 
 const Calendar = () => {
-  const day = (new Date()).getDay()
-  let today = new Date()
+  let today = new Date();
 
-  const data = []
+  const data = [];
+  data.push({ title: weekdays[today.getDay()], date: today.getDate(), isToday: true });
 
-  for (let i = 0; i < 5; i++) {
-    today = new Date(today.getTime() + 24 * 60 * 60 * 1000)
-    data.push({ title: weekdays[today.getDay()], date: today.getDate(), isToday: i === 0 })
-    // console.log(today)
+  for (let i = 0; i < 4; i++) {
+    today = new Date(today.getTime() + 24 * 60 * 60 * 1000);
+    data.push({ title: weekdays[today.getDay()], date: today.getDate(), isToday: false });
   }
 
-  console.log(day === 3 ? 'white' : 'red')
   return (
     <Week>
-      {data.map(item => <Circle color={data.isToday ? 'red' : 'white'} key={data.date}> {item.title} <br />{item.date} </Circle>)}
+      {data.map((item) => (
+        <Circle color={item.isToday ? "#ED6519" : "white"} key={item.date}>
+          {" "}
+          {item.title} <br />
+          {item.date}{" "}
+        </Circle>
+      ))}
     </Week>
-  )
-}
+  );
+};
 
-export default Calendar
+export default Calendar;
 
 // get current date
