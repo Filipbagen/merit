59cb6f8281c764fa28c1ab2845f46ff69d630956
Oliver Lundin <79059027+olvard@users.noreply.github.com>
2021-12-02 16:11:10 +0100

    fixed console errors

--- src/components/Progressbar.js
+++ src/components/Progressbar.js
@@ -1,27 +1,29 @@
-import React from 'react'
-import './index-progressbar.css'
+import React from "react";
+import "./index-progressbar.css";
 
 const Progressbar = () => {
-  const startDate = new Date('09/09/2020')
-  const endDate = new Date('06/06/2025')
-  const totDays = (endDate.getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24)
+  const startDate = new Date("09/09/2020");
+  const endDate = new Date("06/06/2025");
+  const totDays = (endDate.getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24);
 
-  const compleated = Math.floor(((new Date().getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24)))
-  const percentageDiff = ((compleated / totDays).toFixed(2)) * 100
+  const compleated = Math.floor((new Date().getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24));
+  const percentageDiff = (compleated / totDays).toFixed(2) * 100;
 
-  const [value, setValue] = React.useState(0)
+  const [value, setValue] = React.useState(0);
   React.useEffect(() => {
-    setValue(percentageDiff)
-  })
+    setValue(percentageDiff);
+  }, [percentageDiff]);
 
   return (
     <div>
-      <h3 className='percent-number' style={{ fontSize: '23px', fontFamily: 'Barlow', textDecoration: 'underline' }}>Klart av MT: {percentageDiff}%</h3>
-      <div className='progress-div'>
-        <div style={{ width: `${value}%` }} className='progress' />
+      <h3 className="percent-number" style={{ fontSize: "23px", fontFamily: "Barlow", textDecoration: "underline" }}>
+        Klart av MT: {percentageDiff}%
+      </h3>
+      <div className="progress-div">
+        <div style={{ width: `${value}%` }} className="progress" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Progressbar
+export default Progressbar;
--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,63 +1,67 @@
-import { useEffect, useState } from 'react'
-import Chart from './Chart'
-import { PropagateLoader } from 'react-spinners'
-import styled from 'styled-components'
-import { v4 as uuidv4 } from 'uuid'
+import { useEffect, useState } from "react";
+import Chart from "./Chart";
+import { PropagateLoader } from "react-spinners";
+import styled from "styled-components";
+import { v4 as uuidv4 } from "uuid";
 
 // array med alla modules
 
-const moduleTypes = ['TEN1', 'DAT1', 'KTR1', 'KTR2', 'KTR3', 'PRA1']
+const moduleTypes = ["TEN1", "DAT1", "KTR1", "KTR2", "KTR3", "PRA1"];
 
 const Loader = styled.div`
-padding-left: 10px;
-min-width: 100vw;
-overflow: auto;
-display: flex;
-&::-webkit-scrollbar {
-  display: none;
-}
-`
+  padding-left: 10px;
+  min-width: 100vw;
+  overflow: auto;
+  display: flex;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
 
 const Statistics = ({ courseCode }) => {
-  const [data, setData] = useState([])
+  const [data, setData] = useState([]);
 
   const fetchStatics = async () => {
-    let resData = []
-    let index = 0
+    let resData = [];
+    let index = 0;
 
     do {
       // console.log(courseCode, 'courseCode')
-      const url = new URL('https://liu-server.herokuapp.com/statistics')
+      const url = new URL("https://liu-server.herokuapp.com/statistics");
       // console.log('looking for: ' + moduleTypes[index])
-      const params = { course: courseCode, module: moduleTypes[index] }
-      url.search = new URLSearchParams(params).toString()
-      const res = await window.fetch(url)
-      resData = await res.json()
-      index++
+      const params = { course: courseCode, module: moduleTypes[index] };
+      url.search = new URLSearchParams(params).toString();
+      const res = await window.fetch(url);
+      resData = await res.json();
+      index++;
       if (index > moduleTypes.length) {
-        window.alert('Ingen statistik hittades')
-        break
+        window.alert("Ingen statistik hittades");
+        break;
       }
       // console.log(resData)
-    } while (resData.length === 0)
+    } while (resData.length === 0);
 
-    setData(resData.map(item => {
-      return { ...item, id: uuidv4() }
-    }))
+    setData(
+      resData.map((item) => {
+        return { ...item, id: uuidv4() };
+      })
+    );
 
     // console.log(resData)
-  }
+  };
 
   useEffect(() => {
-    fetchStatics()
-  }, [])
+    fetchStatics();
+  });
 
   return (
     <Loader>
-      {data.length === 0 && <PropagateLoader color='#ED6519' loading size={20} />}
-      {data.slice(0, 5).map(item => <Chart data={item} key={item.id} />)}
+      {data.length === 0 && <PropagateLoader color="#ED6519" loading size={20} />}
+      {data.slice(0, 5).map((item) => (
+        <Chart data={item} key={item.id} />
+      ))}
     </Loader>
-  )
-}
+  );
+};
 
-export default Statistics
+export default Statistics;
