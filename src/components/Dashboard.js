fa8a3db21219277336fd8208b95a2cf26ad6986d
Oliver Lundin <79059027+olvard@users.noreply.github.com>
2021-12-03 13:14:27 +0100

    affordans

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,38 +1,38 @@
-import React from 'react'
-import Message from './Message'
-import Quotes from './Quotes'
-import Progressbar from './Progressbar'
-import styled from 'styled-components'
-import Calendar from './Calendar'
-import CourseMap from './CourseMap'
-import Exmap from './Exmap'
+import React from "react";
+import Message from "./Message";
+import Quotes from "./Quotes";
+import Progressbar from "./Progressbar";
+import styled from "styled-components";
+import Calendar from "./Calendar";
+import CourseMap from "./CourseMap";
+import Exmap from "./Exmap";
 // import Background from './Background'
 
 const Content = styled.div`
-{
-  top: 15px;
-  min-height: 80vh;
-  position: relative;
-  background-color: white;
-  border-radius: 44px 0 0 0;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-evenly;
-  box-shadow: 0px 0px 30px 8px rgba(0, 0, 0, 0.1);
-  -webkit-box-shadow: 0px 0px 30px 8px rgba(0, 0, 0, 0.1);
-  align-items: center;
-}
-&:after {
-  content: "";
-  position: absolute;
-  top: -100px;
-  right: 0;
-  height: 100px;
-  width: 100px;
-  border-bottom-right-radius: 50%;
-  box-shadow: 0 50px 0 0 #fff;
-}
-`
+   {
+    top: 15px;
+    min-height: 80vh;
+    position: relative;
+    background-color: white;
+    border-radius: 44px 0 0 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    box-shadow: 0px 0px 30px 8px rgba(0, 0, 0, 0.1);
+    -webkit-box-shadow: 0px 0px 30px 8px rgba(0, 0, 0, 0.1);
+    align-items: center;
+  }
+  &:after {
+    content: "";
+    position: absolute;
+    top: -100px;
+    right: 0;
+    height: 100px;
+    width: 100px;
+    border-bottom-right-radius: 50%;
+    box-shadow: 0 50px 0 0 #fff;
+  }
+`;
 
 const Timer = styled.a`
   border-radius: 10px;
@@ -46,37 +46,34 @@ const Timer = styled.a`
   line-height: 20px;
   text-decoration: none;
   margin: 20px;
-`
+`;
 
 const Container = styled.div`
   min-width: 100vw;
   min-height: 100vh;
   background-color: #d7c0d0;
   justify-content: space-evenly;
-`
+`;
 
 const Courses = styled.div`
   width: 100vw;
   overflow: auto;
   display: flex;
-`
+`;
 
 const Text = styled.p`
-font-size: 23px; 
-padding: 0 0 0 20px;
-font-family: Barlow;
-align-self: flex-start;
-color: rgb(60, 60, 60);
-margin: 35px 0px 0px;
-`
-const Quote = styled.div`
-
-`
+  font-size: 23px;
+  padding: 0 0 0 20px;
+  font-family: Barlow;
+  align-self: flex-start;
+  color: rgb(60, 60, 60);
+  margin: 35px 0px 0px;
+`;
+const Quote = styled.div``;
 
 const Dashboard = () => {
   return (
     <Container>
-
       <Message />
       <Calendar />
 
@@ -86,12 +83,10 @@ const Dashboard = () => {
           <CourseMap />
         </Courses>
 
-
         <Exmap />
 
-
-        <Timer href='pomodoro'>
-          <h2>TIMER</h2>
+        <Timer href="pomodoro">
+          <h2>POMODORO</h2>
         </Timer>
 
         <Progressbar />
@@ -99,10 +94,9 @@ const Dashboard = () => {
         <Quote>
           <Quotes />
         </Quote>
-
       </Content>
     </Container>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
